887af752c1b433b1b20a470bfeb15f744147f891
Alexei Panov <elemc@atisserv.ru>
2011-02-12 15:33:22 +0300

    Changed sources-script to curl download

--- get_sources.sh
+++ get_sources.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-wget http://pkgs.fedoraproject.org/repo/pkgs/firstboot/firstboot-1.113.tar.bz2/85f8a06f03f1e89c0a4ded84a8f3f23a/firstboot-1.113.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/firstboot/firstboot-1.113.tar.bz2/85f8a06f03f1e89c0a4ded84a8f3f23a/firstboot-1.113.tar.bz2
+
