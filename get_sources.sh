1afb1b5683e40ec43029676e00b71ee744439002
Tigro <ashejn@gmail.com>
2013-04-09 16:30:30 +0400

    new source

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/firstboot/firstboot-18.6.tar.gz/7fb7d1e5fc4c0c9cb9088c761ddbba5a/firstboot-18.6.tar.gz
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/firstboot/firstboot-18.7.tar.gz/2494f9918654ae4c5fe49a7a5c2467a8/firstboot-18.7.tar.gz
 
